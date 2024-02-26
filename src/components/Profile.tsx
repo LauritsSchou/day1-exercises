9b512768841cbb1f4d141067c0bc7ad6da74c6f7
LauritsSchou <laurits.schou@gmail.com>
2024-02-26 11:32:39 +0100

    solved exercise 1

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,22 @@
-export default function Profile() {
+type ProfileProps = {
+  name: string;
   email: string;
   isActive: boolean;
   singleLine: boolean;
-  name: string;
+};
+
+export default function Profile({ email, isActive, name, singleLine }: ProfileProps) {
+  if (singleLine)
+    return (
+      <p>
+        Name: {name} Email: {email} {isActive ? "Aktiv" : "Inaktiv"}
+      </p>
+    );
+  return (
+    <>
+      <p>Name: {name}</p>
+      <p>Email: {email}</p>
+      <p>{isActive ? "Aktiv" : "Inaktiv"}</p>
+    </>
+  );
 }
--- src/exercises/PropsDemo.tsx
+++ src/exercises/PropsDemo.tsx
@@ -1,14 +1,16 @@
 import { BaseProps } from "../types";
-import { Profile } from "../components/Profile";
+import Profile from "../components/Profile";
 import { useState } from "react";
 
-export default function Propsdemo({ title }: BaseProps) {
+export default function PropsDemo({ title }: BaseProps) {
   const [showHorizontal, setShowHorizontal] = useState(false);
   return (
     <>
       <h2>{title}</h2>
       Direction: <input type="checkbox" checked={showHorizontal} onChange={() => setShowHorizontal(!showHorizontal)} />
       <Profile name="Max Power" email="mp@email.com" isActive={true} singleLine={showHorizontal} />
+      <Profile name="Michael Motorcycle" email="mm@email.com" isActive={false} singleLine={showHorizontal} />
+      <Profile name="Mickey Mouse" email="mmouse@email.com" isActive={true} singleLine={showHorizontal} />
       {/** Add two or more Profiles, but not until you actually have implemented <Profile />   */}
     </>
   );
